d7ef5ec7c2058a77bb7926b118a9b060b6afe483
ssskkk529 <2472290830@qq.com>
2026-09-09 21:21:01 +0800

    add sw.js

---
+++ sw.js
@@ -0,0 +1,39 @@
+/* 麦抢 PWA Service Worker */
+const CACHE = 'maiqiang-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './css/style.css',
+  './js/data.js',
+  './js/app.js',
+  './manifest.json',
+  './icon.svg'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  // 海报 API 走网络优先（失败回缓存）
+  const url = new URL(e.request.url);
+  if (url.origin !== location.origin) {
+    e.respondWith(
+      fetch(e.request).then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      }).catch(() => caches.match(e.request))
+    );
+    return;
+  }
+  // 本地资源：缓存优先
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
